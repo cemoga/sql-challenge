8707cf222aea2d7c0ec1f0e9fa8bbb46739e5a1a
Cesar <cemoga@yahoo.com>
2019-10-11 11:55:37 -0400

    Update config.py - Setup.py

--- config.py - Setup.py
+++ config.py - Setup.py
@@ -1,5 +1,7 @@
 # Connection Data
 
+## Rename this file to config.py and update your Connection Data
+
 ServerName = "YOUR SERVERNAME (EXAMPLE:"localhost")"
 UserName = "YOUR USERNAME"
 Password = "YOUR PASSOWRD"
