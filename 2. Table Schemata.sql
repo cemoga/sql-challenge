6a7bc4d6389328266a3ecf5451912bc7bbfbef8f
Cesar <cemoga@yahoo.com>
2019-10-11 13:32:54 -0400

    Schemata diagram Changed

--- 2. Table Schemata.sql
+++ 2. Table Schemata.sql
@@ -1,78 +1,55 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/HvMhpc
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+﻿DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employees;
 
 
-DROP TABLE IF EXISTS "dept_emp";
-DROP TABLE IF EXISTS "dept_manager";
-DROP TABLE IF EXISTS "salaries";
-DROP TABLE IF EXISTS "titles";
-DROP TABLE IF EXISTS "departments";
-DROP TABLE IF EXISTS "employees";
-
-
-CREATE TABLE "departments" (
-    "dept_no" char(4)   NOT NULL,
-    "dept_name" varchar   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
+CREATE TABLE departments (
+    dept_no char(4)   NOT NULL PRIMARY KEY,
+    dept_name varchar   NOT NULL
 );
 
-CREATE TABLE "dept_emp" (
-    "emp_no" int   NOT NULL,
-    "dept_no" char(4)   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+CREATE TABLE employees (
+    emp_no int   NOT NULL PRIMARY KEY,
+    birth_date date   NOT NULL,
+    first_name varchar   NOT NULL,
+    last_name varchar   NOT NULL,
+    gender char(1)   NOT NULL,
+    hire_date date   NOT NULL
 );
 
-CREATE TABLE "dept_manager" (
-    "dept_no" char(4)   NOT NULL,
-    "emp_no" int   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+CREATE TABLE dept_emp (
+    emp_no int   NOT NULL,
+    FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+    dept_no char(4)   NOT NULL,
+    FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,
+    PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
-    "birth_date" date   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
-    "gender" char(1)   NOT NULL,
-    "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
+CREATE TABLE dept_manager (
+    dept_no char(4)   NOT NULL,
+    emp_no int   NOT NULL,
+    FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
 );
 
-CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+CREATE TABLE salaries (
+    emp_no int   NOT NULL,
+    FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+    salary int   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
 );
 
-CREATE TABLE "titles" (
-    "emp_no" int   NOT NULL,
-    "title" varchar   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+CREATE TABLE titles (
+    emp_no int   NOT NULL,
+    FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+    title varchar   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
 );
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
